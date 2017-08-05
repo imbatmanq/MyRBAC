3478854841dd7f6cc5b74f2dfe7d23e017a6e4a4
Pantyz <imbatmanqz@gmail.com>
2017-08-05 15:48:55 +0700

    MainFragment3

--- app/src/main/java/com/rbac/jirayu/myrbac/MainActivity.java
+++ app/src/main/java/com/rbac/jirayu/myrbac/MainActivity.java
@@ -3,11 +3,25 @@ package com.rbac.jirayu.myrbac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rbac.jirayu.myrbac.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContainer, mainFragment)
+                    .commit();
+        }
+
+    }   // Main Method
+
+}   // Main Class
+
